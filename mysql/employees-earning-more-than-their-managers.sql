ebd5272f70aefe0cd302baffc9bb491c0b7c603c
Chakming <chakming_wong@executivecentre.com>
2015-07-15 15:49:04 +0800

    mysql challenge - Employees Earning More Than Their Managers - faster version

--- mysql/employees-earning-more-than-their-managers.sql
+++ mysql/employees-earning-more-than-their-managers.sql
@@ -1,5 +1,4 @@
 SELECT e1.NAME
 FROM Employee e1
-LEFT OUTER JOIN Employee e2 ON e1.ManagerId = e2.Id
+INNER JOIN Employee e2 ON e1.ManagerId = e2.Id
 	AND e1.Salary > e2.Salary
-WHERE e2.Id IS NOT NULL
